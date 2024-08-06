ab04dfd6f312c98b227109ef2d1f48ee71f90dd9
LeonardoBigelli <l.bigelli2@campus.uniurb.it>
2024-08-06 12:31:01 +0200

    Introduzione Stream di risultati

--- lib/consumer.dart
+++ lib/consumer.dart
@@ -3,8 +3,13 @@ import 'package:ku_har_app/interpreter.dart';
 
 //class to listen the data stream and do inference on the data
 class Consumer {
+  //stream vero e proprio
   late Stream<List<List<dynamic>>> _sensorDataStream;
+  //sottoscrizione allo stream
   late StreamSubscription<List<List<dynamic>>> _streamHandling;
+  // stream per i risultati
+  final StreamController<int> _streamResults =
+      StreamController<int>.broadcast();
   late InterpreterTf interpreter;
   late int _inferenceResult;
 
@@ -20,6 +25,7 @@ class Consumer {
   void _doInference(List<List<dynamic>> window) {
     interpreter.runIference(window);
     _inferenceResult = interpreter.output;
+    _streamResults.add(_inferenceResult);
   }
 
   //getter
